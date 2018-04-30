40505cfa6540d17121f7930ed9bead5d4d40a64d
Dan Martin <dmartin@fh.org>
2018-04-29 20:50:00 -0700

    set up test db environment, chapter 156

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from "firebase";
 
 const config = {
-    apiKey: "AIzaSyAkrw_I-wjdzRjMWYJOBLPgN7rKeh9LdvI",
-    authDomain: "expensify-20319.firebaseapp.com",
-    databaseURL: "https://expensify-20319.firebaseio.com",
-    projectId: "expensify-20319",
-    storageBucket: "expensify-20319.appspot.com",
-    messagingSenderId: "526611733821"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
   
   firebase.initializeApp(config);
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -2,6 +2,7 @@ import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase';
 
 const createMockStore = configureMockStore([thunk]);
 
@@ -33,7 +34,7 @@ test('should setup addExpense object with given value', () => {
     });
 });
 
-test('should add expense to database and store', () => {
+test('should add expense to database and store', (done) => {
     const store = createMockStore({});
     const expenseData = {
         description: 'Mouse',
@@ -41,10 +42,48 @@ test('should add expense to database and store', () => {
         note: 'My note',
         createdAt: 102022882828
     };
-    store.dispatch(startAddExpense(expenseData));
+    store.dispatch(startAddExpense(expenseData)).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...expenseData
+            }
+        });
+
+        return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual(expenseData);
+        done();
+    }).catch((e) => {
+        console.log('error', e);
+    });
 });
 
-test('should add expense with defaults to database and store', () => {
+test('should add expense with defaults to database and store', (done) => {
+    const store = createMockStore({});
+    const expenseData = {
+        amount: 0,
+        createdAt: 0,
+        description: '',
+        note: '' 
+    };
+    store.dispatch(startAddExpense({})).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual({
+            type: 'ADD_EXPENSE',
+            expense: {
+                id: expect.any(String),
+                ...expenseData
+            }
+        });
+
+        return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+    }).then((snapshot) => {
+        expect(snapshot.val()).toEqual(expenseData);
+        done();
+    });
     
 });
 
