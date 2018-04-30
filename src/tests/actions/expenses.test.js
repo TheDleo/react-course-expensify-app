6151b5ecfe93407e240150cb6e6da13f0b93b38d
Dan Martin <dan@dleo.info>
2018-04-29 17:47:46 -0700

    chapter 153 WIP

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,5 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import { startAddExpense, addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import expenses from '../fixtures/expenses';
 
 test('should setup removeExpense action object', () => {
     const action = removeExpense( { id: '123abc' } );
@@ -20,35 +21,27 @@ test('should return correct editExpense action object', () => {
 });
 
 test('should setup addExpense object with given value', () => {
-    const expectedData = {
-        description: 'Test description',
-        note: 'a note\n here.',
-        amount: 100,
-        createdAt: 1000
-    };
-    const result = addExpense(expectedData);
+    const expense = expenses[1];
+    const result = addExpense(expense);
     expect(result).toEqual({
         type: 'ADD_EXPENSE',
-        expense: {
-            ...expectedData,
-            id: expect.any(String)
-        }
+        expense
     });
 });
 
-test('should setup addExpense object with default values', () => {
-    const expectedData = {
-        description: '',
-        note: '',
-        amount: 0,
-        createdAt: 0
-    };
-    const result = addExpense();
-    expect(result).toEqual({
-        type: 'ADD_EXPENSE',
-        expense: {
-            ...expectedData,
-            id: expect.any(String)
-        }
-    });
-});
+// test('should setup addExpense action object with default values', () => {
+//     const expectedData = {
+//         description: '',
+//         note: '',
+//         amount: 0,
+//         createdAt: 0
+//     };
+//     const result = addExpense();
+//     expect(result).toEqual({
+//         type: 'ADD_EXPENSE',
+//         expense: {
+//             ...expectedData,
+//             id: expect.any(String)
+//         }
+//     });
+// });
