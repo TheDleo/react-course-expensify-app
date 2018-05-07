1adcdfb6c2788668e29ce70f491ef66fd25c5dbb
Dan Martin <dmartin@fh.org>
2018-05-06 20:39:37 -0700

    chapter 159, remove expense from firebase

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -27,6 +27,16 @@ export const removeExpense = ( {id} = {} ) => {
     id
 }};
 
+export const startRemoveExpense = ( {id} = {}) => {
+    return (dispatch) => {
+        return database.ref(`expenses/${id}`).remove().then(() => {
+            dispatch(removeExpense({id}));
+        }).catch((e) => {
+            console.log('Error in remove expense',e);
+        });
+    };
+};
+
 export const editExpense = ( id, updates ) => ({
     type: 'EDIT_EXPENSE',
     id,
